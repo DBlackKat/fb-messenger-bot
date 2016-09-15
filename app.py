9e99b10d8bb13427e6b2e96705e6df2d31acc9ea
DBlackKat <zhirui09400@gmail.com>
2016-09-15 22:37:35 +0800

    test

--- app.py
+++ app.py
@@ -29,19 +29,23 @@ def webhook():
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
     if data["object"] == "page":
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    if message_text == 'status':
-                        send_message(sender_id,"no washing machine available")
-                    else:
-                        send_message(sender_id, "got it, thanks!")
+                    if messaging_event.get("attachments"):
+                        for image in messaging_event["message"]["attachments"]:
+                            img = image["payload"]["url"]
+                            send_message(sender_id, "got it, thanks!")
+                    if messaging_event.get("text"):
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+
+                        if message_text == 'status':
+                            send_message(sender_id,"no washing machine available")
+                        else:
+                            send_message(sender_id, "got it, thanks!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
